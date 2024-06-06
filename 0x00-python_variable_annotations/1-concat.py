12fcf3c124577febeb1dc084933d22c97b0d53e1
weaver010 <abdelrahmannagaty839@gmail.com>
2024-06-06 15:50:09 +0000

    hi

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ returns their concatenation"""
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function floor"""
+
+
+def floor(n: float) -> int:
+    """Returns floor of a float passed to it"""
+    return int(n)
